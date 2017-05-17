f4521c34d42e3b785538e815b4ea584e7dd8f219
snlu <495716110@qq.com>
2017-05-17 15:44:49 +0800

    springBoot基础框架

---
+++ SpringBoot/src/main/java/com/boot/Application.java
@@ -0,0 +1,16 @@
+package com.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by snlu on 2017/5/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
