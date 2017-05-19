29108462255214501efcd71720de8ee72b2e7c98
snlu <495716110@qq.com>
2017-05-19 17:24:56 +0800

    spring boot activty mq

--- SpringBoot/src/main/java/com/boot/Application.java
+++ SpringBoot/src/main/java/com/boot/Application.java
@@ -1,8 +1,14 @@
 package com.boot;
 
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.core.JmsMessagingTemplate;
+
+import javax.jms.Queue;
 
 /**
  * Created by snlu on 2017/5/16.
@@ -13,4 +19,23 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public Queue queue(){
+        return new ActiveMQQueue("queue1");
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory();
+    }
+
+    @Bean
+    public JmsMessagingTemplate jmsMessagingTemplate(ActiveMQConnectionFactory connectionFactory){
+        System.out.println("get JmsMessagingTemplate");
+        return new JmsMessagingTemplate(connectionFactory);
+    }
+
 }
+
+
+
